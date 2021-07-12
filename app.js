d84c53ad5f0156b55b2fc7c0513f50ddf81ae2a7
harrymcg <harry.mcguire2@gmail.com>
2021-07-12 11:40:17 +1000

    Working buttons using JS.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 //Testing whether I've created as a new branch
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Make it dark?";
+    }
+    else {
+        this.textContent = "Make it light?";
+    }
+    console.log('current class name: ' + className)
+});
